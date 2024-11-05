678b9768c78734ffd133cfd48a6c634994122800
Isaac Sibaja <isaacsibaja901@gmail.com>
2024-11-05 01:31:31 -0600

    se crea clase comida con sus atrubutos

---
+++ Comida.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sibaj
+ */
+public class Comida {
+     private int comidaX;
+    private int comidaY;
+
+    public Comida() {
+    }
+
+    public int getComidaX() {
+        return comidaX;
+    }
+
+    public void setComidaX(int comidaX) {
+        this.comidaX = comidaX;
+    }
+
+    public int getComidaY() {
+        return comidaY;
+    }
+
+    public void setComidaY(int comidaY) {
+        this.comidaY = comidaY;
+    }
+    
+}
